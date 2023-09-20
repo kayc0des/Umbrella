f86dac78406997c18c8d5513828268514d2add6a
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-09-20 09:54:14 +0200

    basemodel class updated

--- umbrella-app/models/basemodel.py
+++ umbrella-app/models/basemodel.py
@@ -2,23 +2,30 @@ import uuid
 import datetime
 
 class BaseModel():
-    # Include all attributes and methods for all data objects
+    """BaseModel Class for all umbrella Data Models"""
 
-    def __init__(self):
-        # Constructor method called when an instance of the BaseModel is created
+    def __init__(self, **kwargs):
+        """Constructor method called when an instance of the BaseModel is created"""
         self.id = uuid.uuid4()
-        self.created_at = datetime.datetime.now().strftime("%c")
+        self.created_at = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = self.created_at
 
-    def updated_at(self):
-        self.created_at = datetime.datetime.now().strftime("%c")
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
-    def __repr__(self):
-        return (f"{self.__class__.__name__}: id: {self.id} timestamp: {self.created_at}")
+    def save(self):
+        # Updates the created at time
+        self.updated_at = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+    def __str__(self):
+        # for debugging
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.created_at}")
 
     def to_dict(self):
-        pass
+        bm_dict = self.__dict__
+        bm_dict[__class__] = self.__class__.__name__
     
-# Test
+# Debug
 model = BaseModel()
-print(f"The unique identifier for model is: {model.id} and it was created at {model.created_at}")
-print(model.__repr__())
+model.to_dict()
